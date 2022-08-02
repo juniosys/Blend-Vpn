9a85fa0415084e23dc1bf7c368ca696bfccc2586
fjunio97 <flaviojunio1997@gmail.com>
2022-08-01 21:10:35 -0300

    implement Protocol TLS v1.3

--- app/src/main/java/com/slipkprojects/sockshttp/tunnel/TLSSocketFactory.java
+++ app/src/main/java/com/slipkprojects/sockshttp/tunnel/TLSSocketFactory.java
@@ -10,6 +10,7 @@ import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
@@ -21,32 +22,68 @@ import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import android.annotation.SuppressLint;
+
+import org.conscrypt.Conscrypt;
+
 import java.security.SecureRandom;
+
 
 public class TLSSocketFactory extends SSLSocketFactory {
     private SSLSocketFactory internalSSLSocketFactory;
 
+
+
+    static {
+        try {
+            Security.insertProviderAt(Conscrypt.newProvider(), 1);
+
+        } catch (NoClassDefFoundError e) {
+            e.printStackTrace();
+        }}
+
+    public SSLContext sslctx;
+
     public TLSSocketFactory(InputStream certStream) throws KeyManagementException, NoSuchAlgorithmException, IOException, CertificateException, KeyStoreException {
-        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+
+        X509TrustManager tm = Conscrypt.getDefaultX509TrustManager();
+
+        CertificateFactory cf = CertificateFactory.getInstance("Conscrypt");
+        //(null, new TrustManager[] { tm }, null);
+
+
+        //CertificateFactory cf = CertificateFactory.getInstance("X.509");
         InputStream caInput = new BufferedInputStream(certStream);
         try {
             Certificate ca = cf.generateCertificate(caInput);
-            System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+            System.out.println("tm=" + ((X509Certificate) tm).getSubjectDN());
             KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
             keyStore.load(null, null);
-            keyStore.setCertificateEntry("ca", ca);
+            keyStore.setCertificateEntry("tm", ca);
             TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
             tmf.init(keyStore);
+
             SSLContext sslctx = SSLContext.getInstance("TLS");
+            //tentar ativar amanha
+			//  SSLContext sslctx = SSLContext.getInstance("TLS", "Conscrypt");
             sslctx.init(null, tmf.getTrustManagers(), null);
             this.internalSSLSocketFactory = sslctx.getSocketFactory();
         } finally {
             caInput.close();
         }
     }
-	
-	public TLSSocketFactory() throws KeyManagementException, NoSuchAlgorithmException {
+
+
+    public TLSSocketFactory() throws KeyManagementException, NoSuchAlgorithmException {
         // For easier debugging purpose, trust all certificates
+
+
+
+
+
+
+
+        //dsp = ApplicationBase.getDefSharedPreferences();
         TrustManager[] trustAllCerts = new TrustManager[]{
 			new X509TrustManager() {
 				public X509Certificate[] getAcceptedIssuers() {
@@ -62,49 +99,69 @@ public class TLSSocketFactory extends SSLSocketFactory {
 				}
 			}
         };
-        SSLContext context = SSLContext.getInstance("TLS");
-        context.init(null, trustAllCerts, new SecureRandom());
-        internalSSLSocketFactory = context.getSocketFactory();
+        // SSLContext protocols: TLS, SSL, SSLv3
+        //SSLContext sc = SSLContext.getInstance("SSLv3");
+        //System.out.println("\nSSLContext class: "+sc.getClass());
+        //System.out.println("   Protocol: "+sc.getProtocol());
+        //System.out.println("   Provider: "+sc.getProvider());
+
+        sslctx = SSLContext.getInstance("TLS");
+        sslctx.init(null, trustAllCerts, new SecureRandom());
+        internalSSLSocketFactory = sslctx.getSocketFactory();
+
     }
 
+    @Override
     public String[] getDefaultCipherSuites() {
-        return this.internalSSLSocketFactory.getDefaultCipherSuites();
+        return internalSSLSocketFactory.getDefaultCipherSuites();
     }
 
+    @Override
     public String[] getSupportedCipherSuites() {
-        return this.internalSSLSocketFactory.getSupportedCipherSuites();
+        return internalSSLSocketFactory.getSupportedCipherSuites();
     }
 
+    @Override
     public Socket createSocket() throws IOException {
-        return enableTLSOnSocket(this.internalSSLSocketFactory.createSocket());
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket());
     }
 
+    @Override
     public Socket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
-        return enableTLSOnSocket(this.internalSSLSocketFactory.createSocket(s, host, port, autoClose));
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(s, host, port, autoClose));
     }
 
+    @Override
     public Socket createSocket(String host, int port) throws IOException, UnknownHostException {
-        return enableTLSOnSocket(this.internalSSLSocketFactory.createSocket(host, port));
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port));
     }
 
+    @Override
     public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException, UnknownHostException {
-        return enableTLSOnSocket(this.internalSSLSocketFactory.createSocket(host, port, localHost, localPort));
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port, localHost, localPort));
     }
 
+    @Override
     public Socket createSocket(InetAddress host, int port) throws IOException {
-        return enableTLSOnSocket(this.internalSSLSocketFactory.createSocket(host, port));
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(host, port));
     }
 
+    @Override
     public Socket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort) throws IOException {
-        return enableTLSOnSocket(this.internalSSLSocketFactory.createSocket(address, port, localAddress, localPort));
+        return enableTLSOnSocket(internalSSLSocketFactory.createSocket(address, port, localAddress, localPort));
     }
 
+    //	private Socket enableTLSOnSocket(Socket socket) {
+//		if(socket instanceof SSLSocket) ((SSLSocket) socket).setEnabledProtocols(new String[] {"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"});
+//		return socket;
+//
     private Socket enableTLSOnSocket(Socket socket) {
-        if (socket != null && (socket instanceof SSLSocket)) {
-            ((SSLSocket) socket).setEnabledProtocols(new String[]{"TLSv1"});
+
+        if (socket instanceof SSLSocket) {
+            ((SSLSocket) socket).setEnabledProtocols(((SSLSocket) socket).getSupportedProtocols());
+            ((SSLSocket) socket).setEnabledProtocols(new String[] {"TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3"});
         }
         return socket;
     }
-	
-}
 
+}
